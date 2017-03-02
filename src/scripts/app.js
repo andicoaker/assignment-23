183b16dce6125848ef7c235ffa1e834c11aa28d7
andicoaker <andicoaker@gmail.com>
2017-03-01 23:03:37 -0500

    initial files, started react in app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,54 @@
-import moment from 'moment'
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-let m = moment(Date.now()).year()
+let appContainerEl = document.querySelector('#app-container')
+
+const CongressPeepsProps = React.createClass({
+
+  render: function(){
+    console.log('props.congressPeeps', this.props.congressPeeps)
+
+    return (
+      <div>
+        <p>  Yoooooo!!! </p>
+        <SingleCongressPeepsProfile someCongressPeeps={this.props.congressPeeps.results}/>
+        {/* map over data and insert single CongressPeeps template here, then return the single CongressPeeps template */}
+
+      </div>
+
+    )
+  }
+})
+
+const SingleCongressPeepsProfile = React.createClass({
+
+  _createSingleCongressPeepsJSX: function(arrOfPeeps){
+
+    let jsxArr = arrOfPeeps.map(function(peepsObj){
+      return (
+
+        <div>
+          <h3> firstName  lastName  </h3>
+          <h4> title  --  party - stateName </h4>
+          <ul>'
+            <li> email: email </li>
+            <li> website: website </li>
+            <li> facebook: facebook </li>
+            <li> twitter: twitter </li>
+          </ul>
+          <p>Term End + termEnd </p>
+        </div>
+
+      )
+
+    })
+  }
+})
 
 
-{ userName: 'Honey Bear' ,  lastLoggedIn: 1488335292232 },
-{ userName: 'Lazy Philosopher' ,  lastLoggedIn: 1488248962768 },
-{ userName: 'HRG',  lastLoggedIn: 1488076247719 },
-{ userName: 'ChirpChirp',  lastLoggedIn: 1488076190847 },
-{ userName: 'Babushka',  lastLoggedIn: 1488007366792 },
-{ userName: 'Oh Mia',  lastLoggedIn: 1486694105551 }
 
+$.getJSON('https://congress.api.sunlightfoundation.com/legislators?callback=?').then(function(serverRes){
+  ReactDOM.render(
+  <CongressPeepsProps congressPeeps={serverRes}/> , appContainerEl);
+})
